903b2dcdcd7a509a53046bb25f70270f0968e838
Bruno Arruda <arrudabruno10@gmail.com>
2021-06-07 01:04:20 -0300

    feat(DiscordBot): cria tasks básicas no ClickUp

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# Discord Bot doc: https://discordpy.readthedocs.io/en/stable/api.html
+
+import asyncio
 import os
+import clickup
+import traceback
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '$')
@@ -11,8 +16,30 @@ async def on_ready():
     print("bot online")
 
 @client.command()
-async def ping(message):
-    await message.send("pong!")
+async def task(ctx: commands.context.Context, *, args):
+    try:
+        meta, name, description, fields = [x for x in args.split('\n') if x != '']
+        if meta.count('.') == 1:
+            meta = meta.replace('.', '..')
+        space, folder, list = meta.split('.')
+        task = clickup.Task(space, folder, list, name, description, fields)
+        client = clickup.Client(os.getenv('CLICKUP_TOKEN'))
+        task = await client.create_task(task)
+        msg = 'Task #{0[id]} created by {0[creator][username]}. Link: https://app.clickup.com/t/{0[id]}'.format(task)
+    except Exception as e:
+        msg = 'Error: {}'.format(traceback.print_exception(e))
+    await ctx.send(msg)
 
+@client.command()
+async def token(ctx: commands.context.Context, app, value):
+    await ctx.send("{}={}".format(app, value))
 
-client.run(os.getenv("DISCORD_TOKEN")) #get your bot token and make a file called ".env" then inside the file write TOKEN=put your api key here example in env.txt
+@client.event
+async def on_reaction(reaction, user):
+    pass
+
+def main():
+    client.run(os.getenv("DISCORD_TOKEN")) #get your bot token and make a file called ".env" then inside the file write TOKEN=put your api key here example in env.txt
+
+if __name__ == '__main__':
+    main()
